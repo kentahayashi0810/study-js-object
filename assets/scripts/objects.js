9e5bb74048ecb23f386e4044ec0d4dfd90575a1d
kentahayashi <kenta0810hayashi@gmail.com>
2023-06-16 00:05:35 +0200

    Added Rendering function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -9,6 +9,24 @@ const serchButton = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = () => {
+  const movieList = document.getElementById("movie-list");
+  if (movies.length == 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+
+  movieList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.appendChild(movieEl);
+  });
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
@@ -31,7 +49,7 @@ const addMovieHandler = () => {
   };
 
   movies.push(newMovie);
-  console.log(movies);
+  renderMovies();
 };
 
 addMovieButton.addEventListener("click", addMovieHandler);
