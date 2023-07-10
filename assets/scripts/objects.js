fd20fe47115442f58a284a71018601ec36989180
kentahayashi <kenta0810hayashi@gmail.com>
2023-07-10 21:40:47 +0200

    234. "this" and Arrow Functions

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -24,8 +24,9 @@ const renderMovies = (filter = "") => {
     const { info, ...otherProps } = movie;
     console.log(otherProps);
 
-    // const { title: movieTitle } = info;
-    let text = movie.getFormattedTitle() + " - ";
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.call(movie) + " - ";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
